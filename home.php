40c2cecf6113e693fa187c1d63214f5279534d8a
FalseGenius <billionairejackal@gmail.com>
2023-05-19 15:20:50 +0500

    Second commit

--- home.php
+++ home.php
@@ -1,10 +1,12 @@
 <?php 
 
 $email = '';
+$name = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (empty($_POST['email'])) {
         http_response_code(502);
-        print json_encode(["message" => "Email required!"]);
+        print json_encode(["message" => "Email required"]);
     }
 }
+
